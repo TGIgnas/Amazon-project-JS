de75e119c388c26cafa69d009774d556dd5bc13d
Ignas <ign.navickas@gmail.com>
2024-12-03 13:28:33 +0200

    Fix price formating and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formartCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = '';
 
@@ -29,7 +30,7 @@ cart.forEach((cartItem) => {
     ${matchingProduct.name}
     </div>
     <div class="product-price">
-    $${matchingProduct.priceCents / 100}
+    $${formartCurrency(matchingProduct.priceCents)}
     </div>
     <div class="product-quantity">
         <span>
@@ -48,10 +49,10 @@ cart.forEach((cartItem) => {
     <div class="delivery-options-title">
         Choose a delivery option:
     </div>
-
     <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-        name="delivery-option-2">
+        <input type="radio" checked
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
         <div>
         <div class="delivery-option-date">
             Tuesday, June 21
@@ -62,8 +63,9 @@ cart.forEach((cartItem) => {
         </div>
     </div>
     <div class="delivery-option">
-        <input type="radio" checked class="delivery-option-input"
-        name="delivery-option-2">
+        <input type="radio"
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
         <div>
         <div class="delivery-option-date">
             Wednesday, June 15
@@ -74,8 +76,9 @@ cart.forEach((cartItem) => {
         </div>
     </div>
     <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-        name="delivery-option-2">
+        <input type="radio"
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
         <div>
         <div class="delivery-option-date">
             Monday, June 13
@@ -93,5 +96,3 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary')
     .innerHTML = cartSummaryHTML;
-
-console.log(cartSummaryHTML);
