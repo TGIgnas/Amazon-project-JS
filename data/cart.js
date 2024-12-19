6cb376a0438c56b310fc8e87b1afa9c132776799
Ignas <ign.navickas@gmail.com>
2024-12-19 15:19:03 +0200

    Convert from promises to async await

--- data/cart.js
+++ data/cart.js
@@ -109,7 +109,6 @@ export function loadCart(fun) {
     const xhr = new XMLHttpRequest();
   
     xhr.addEventListener('load', () => {
-      console.log(xhr.response);
       fun();
     });
   
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,27 +1,38 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import {renderPaymentSummary} from "./checkout/paymentSummary.js"
 import {renderCheckoutHeader} from "./checkout/checkoutHeader.js";
-import {loadProducts} from "../data/products.js";
+import {loadProducts, loadProductsFetch} from "../data/products.js";
 import {loadCart} from '../data/cart.js';
 
+async function loadPage() {
+  await loadProductsFetch();
 
-Promise.all([ 
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve('value1');
-    });
-  }),
-  new Promise((resolve) => {
+  await new Promise((resolve) => {
     loadCart(() => {
       resolve();
     });
   })
 
-]).then(() => {
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-});
+}
+
+loadPage();
+
+// Promise.all([ 
+//  loadProductsFetch(),
+//   new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   })
+
+// ]).then(() => {
+//   renderCheckoutHeader();
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
 
 
 
