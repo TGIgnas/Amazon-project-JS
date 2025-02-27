558e8623a7b260595fe5687cb03af4371bfc23bd
Ignas <ign.navickas@gmail.com>
2025-02-27 13:51:06 +0200

    Made default cart empty

--- data/cart.js
+++ data/cart.js
@@ -4,119 +4,109 @@ export let cart;
 
 loadFromStorage();
 
-export function loadFromStorage(){
-    cart = JSON.parse(localStorage.getItem('cart'));
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
 
-    if (!cart) {
-        cart = [{
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: '1'
-        }, {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: '2'
-        }];
-    }
-};
+  if (!cart) {
+    cart = [];
+  }
+}
 
 function saveToStorage() {
-    localStorage.setItem('cart', JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-export function addToCart(productId){
-    let matchingItem;
+export function addToCart(productId) {
+  let matchingItem;
 
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-        }
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: "1",
     });
+  }
 
-    if (matchingItem) {
-        matchingItem.quantity += 1;
-    } else {
-        cart.push({
-            productId: productId,
-            quantity: 1,
-            deliveryOptionId: '1'
-        });
-    }
-
-    saveToStorage();
-
-    }
-
-    export function removeFromCart(productId) {
-        const newCart = [];
-
-        cart.forEach((cartItem) => {
-            if(cartItem.productId !== productId) {
-                newCart.push(cartItem);
-            }
-        })
-
-        cart = newCart;
-
-        saveToStorage();
-    }
-
-export function calculateCartQuantity(){
-    let cartQuantity = 0;
-    
-    cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity;
-    });    
-
-    return cartQuantity;
+  saveToStorage();
 }
 
-export function updateQuantity(productId, newQuantity){
+export function removeFromCart(productId) {
+  const newCart = [];
 
-    let matchingItem;
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
 
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId){
-            matchingItem = cartItem
-        };
-    });
+  cart = newCart;
 
-    matchingItem.quantity = newQuantity;
-    
-    saveToStorage();
+  saveToStorage();
+}
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
 }
 
 export function updateDeliveryOption(productId, deliveryOptionId) {
-    let matchingItem;
+  let matchingItem;
 
-    cart.forEach((cartItem) => {
-        if (productId === cartItem.productId){
-            matchingItem = cartItem
-        } else if (!matchingItem) {
-            return;
-        } ;
-    });
-    
-    if (!validDeliveryOption(deliveryOptionId)) {
-        return;
-      }
-    matchingItem.deliveryOptionId = deliveryOptionId;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    } else if (!matchingItem) {
+      return;
+    }
+  });
 
-    saveToStorage();
+  if (!validDeliveryOption(deliveryOptionId)) {
+    return;
+  }
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
 }
 
 export function loadCart(fun) {
-    const xhr = new XMLHttpRequest();
-  
-    xhr.addEventListener('load', () => {
-      fun();
-    });
-  
-    xhr.open('GET', 'https://supersimplebackend.dev/cart');
-    xhr.send();
-  }
+  const xhr = new XMLHttpRequest();
 
-  export function resetCart() {
-    cart.length = 0; 
-    saveToStorage(); 
+  xhr.addEventListener("load", () => {
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+}
+
+export function resetCart() {
+  cart.length = 0;
+  saveToStorage();
 }
